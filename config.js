52894b8730893c1e2721dd41fc5a1f4012529114
OwnerCO <jazminqntro96@gmail.com>
2025-01-05 18:25:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
-	SUDO: process.env.SUDO || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_06_62_17',
+	SUDO: process.env.SUDO || '51960385930',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
 	BOT_INFO: process.env.BOT_INFO || 'αѕтяσχ11;χѕтяσ м∂',
 	STICKER_PACK: process.env.STICKER_PACK || 'мα∂є ву;χѕтяσ мυℓтι ∂єνι¢є вσт',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
-	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
+	TIME_ZONE: process.env.TIME_ZONE || 'America/Lima',
 	DEBUG: process.env.DEBUG || false,
 	VERSION: '1.2.1',
 };
